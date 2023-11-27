ad77400401d61ae0f9b18ca9f917a5ad7b647f33
Dog1144MED <mhoue23@student.aau.dk>
2023-11-27 12:54:50 +0100

    bruh

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -11,15 +13,15 @@ public class DialogueController : MonoBehaviour
 
     public Event DialogueEvent;
     public GameObject textBack;
-    public TMP_Text textTmp;
+    public TMP_Text textSpeech;
 
-
+    int i = 0;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        ShowDialougue("Du er pisse klam at se på. Gå din vej dig", 10);
+        ShowDialougue("Du klam at se på. Gå din vej dig", 10);
     }
 
     // Update is called once per frame
@@ -29,42 +31,70 @@ public class DialogueController : MonoBehaviour
     }
     public void ShowDialougue(string text, int maxChar)
     {
-        textTmp.text = "";
+        textSpeech.text = "";
         string[] words = text.Split(" ");
-        foreach (string word in words)
+        int i = 0;
+        for (i = 0; i < words.Length; i++)
         {
-            if (textTmp.text.Length + word.Length <= maxChar)
+            Debug.Log(textSpeech.text.Length);
+            if (textSpeech.text.Length + words[i].Length <= maxChar)
             {
-                char[] chars = word.ToCharArray();
+                char[] chars = words[i].ToCharArray();
                 foreach (char c in chars)
                 {
-                    textTmp.text = textTmp.text + c;
+                    textSpeech.text = textSpeech.text + c;
                 }
-                textTmp.text += " ";
+                textSpeech.text = textSpeech.text + " ";
             }
-            else if (textTmp.text.Length > maxChar)
+            else if (textSpeech.text.Length + words[i].Length > maxChar)
             {
-                Wait(6);
-                textTmp.text = "";
-                continue;
+                StartCoroutine(WaitForKey());
             }
+
+
+
         }
-        //textTmp.maxVisibleCharacters = 5;
-
-        //figure out length of next word. + space
-
-        //        textTmp.maxVisibleLines = 2;
-        //check next word lenght
-
-        //check how many charcters inside box. //Figure out how many chars fit inside box.
-        //   TMP_Text.textInfo.characterInfo[Index]
 
 
-
+        /*   foreach (string word in words)
+           {
+               if (textTmp.text.Length + word.Length <= maxChar)
+               {
+                   char[] chars = word.ToCharArray();
+                   foreach (char c in chars)
+                   {
+                       textTmp.text = textTmp.text + c;
+                   }
+                   textTmp.text += " ";*/
     }
+    //else if (textTmp.text.Length > maxChar)
+    //{
+    //   Wait(6);
+    // textTmp.text = "";
+    //           continue;
+    //      }
+    // }
+    //textTmp.maxVisibleCharacters = 5;
 
-    IEnumerator Wait(int seconds)
+    //figure out length of next word. + space
+
+    //        textTmp.maxVisibleLines = 2;
+    //check next word lenght
+
+    //check how many charcters inside box. //Figure out how many chars fit inside box.
+    //   TMP_Text.textInfo.characterInfo[Index]
+
+    IEnumerator WaitForKey()
     {
-        yield return new WaitForSeconds(seconds);
+        Debug.Log("Du er homo");
+        yield return new WaitForSeconds(5);
+        Debug.Log("Sike");
+        i--;
+        textSpeech.text = "";
+
+
     }
 }
+
+
+
