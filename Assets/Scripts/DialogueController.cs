48398d83c9b74ce4f1b4a8822ef12b4e8569d802
Dog1144MED <mhoue23@student.aau.dk>
2023-11-27 14:51:05 +0100

    dialogscript er ca færdig

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Threading;
 using TMPro;
 using Unity.Collections.LowLevel.Unsafe;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
@@ -15,84 +18,41 @@ public class DialogueController : MonoBehaviour
     public GameObject textBack;
     public TMP_Text textSpeech;
 
-    int i = 0;
-
+    public string text;
+    public int maxChar;
+    public float charDelay;
     // Start is called before the first frame update
     void Start()
     {
-
-        ShowDialougue("Du klam at se på. Gå din vej dig", 10);
+        StartCoroutine(ShowDialogue(KeyCode.Space, text, maxChar));
     }
-
-    // Update is called once per frame
-    void Update()
+    public IEnumerator ShowDialogue(KeyCode key, string text, int maxChar)
     {
-
-    }
-    public void ShowDialougue(string text, int maxChar)
-    {
+        int i = 0;
         textSpeech.text = "";
         string[] words = text.Split(" ");
-        int i = 0;
+
         for (i = 0; i < words.Length; i++)
         {
-            Debug.Log(textSpeech.text.Length);
+            Debug.Log(words[i]);
             if (textSpeech.text.Length + words[i].Length <= maxChar)
             {
                 char[] chars = words[i].ToCharArray();
                 foreach (char c in chars)
                 {
-                    textSpeech.text = textSpeech.text + c;
+
+                    textSpeech.text = textSpeech.text + c.ToString(); ;
+                    yield return new WaitForSeconds(charDelay);
                 }
                 textSpeech.text = textSpeech.text + " ";
             }
             else if (textSpeech.text.Length + words[i].Length > maxChar)
             {
-                StartCoroutine(WaitForKey());
+                yield return new WaitUntil(() => Input.GetKeyDown(key));
+                textSpeech.text = "";
+                i--;
             }
-
-
-
         }
-
-
-        /*   foreach (string word in words)
-           {
-               if (textTmp.text.Length + word.Length <= maxChar)
-               {
-                   char[] chars = word.ToCharArray();
-                   foreach (char c in chars)
-                   {
-                       textTmp.text = textTmp.text + c;
-                   }
-                   textTmp.text += " ";*/
-    }
-    //else if (textTmp.text.Length > maxChar)
-    //{
-    //   Wait(6);
-    // textTmp.text = "";
-    //           continue;
-    //      }
-    // }
-    //textTmp.maxVisibleCharacters = 5;
-
-    //figure out length of next word. + space
-
-    //        textTmp.maxVisibleLines = 2;
-    //check next word lenght
-
-    //check how many charcters inside box. //Figure out how many chars fit inside box.
-    //   TMP_Text.textInfo.characterInfo[Index]
-
-    IEnumerator WaitForKey()
-    {
-        Debug.Log("Du er homo");
-        yield return new WaitForSeconds(5);
-        Debug.Log("Sike");
-        i--;
-        textSpeech.text = "";
-
-
     }
 }
 
