9ebf5af526b40bdb66884c2ee59446e772ff3f3a
Martin Riber Thomsen <thomsenribermartin@gmail.com>
2023-12-01 13:29:30 +0100

    door fixes

---
+++ Assets/Interactable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Collider2D ObjectCollider;
+
+    public PlayerBehavior player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
